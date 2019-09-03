52e7f8b2dc1a44825f7684a23ef339ee15852afc
Hao Tran <account@haothitran.com>
2019-09-03 00:28:05 -0400

    Flat textures for status bars

--- Modules/RaidStatusBars.lua
+++ Modules/RaidStatusBars.lua
@@ -2,26 +2,10 @@
 -- // MODULES / RAID STATUS BARS
 --------------------------------------------------------------------------------
 
-local function update()
-  local i, bar = 1
-  repeat
-    if bar then
-      bar:SetStatusBarTexture("Interface\\AddOns\\KlazRaidFrames\\Media\\Raid-Bar-Hp-Fill")
-      rbar:SetStatusBarTexture("Interface\\AddOns\\KlazRaidFrames\\Media\\Raid-Bar-Resource-Fill")
-    end
-    i = i + 1
-  until not bar
-end
-
-local f = CreateFrame("Frame")
-f:SetScript("OnEvent",function(self, event, ...)
-
-  if CompactRaidFrameContainer_AddUnitFrame then
-      self:UnregisterAllEvents()
-      hooksecurefunc("CompactRaidFrameContainer_AddUnitFrame", update)
-      hooksecurefunc("CompactUnitFrame_UpdateAll", update)
-      update()
-    end
+hooksecurefunc('DefaultCompactUnitFrameSetup', function (f)
+  f.healthBar:SetStatusBarTexture(.75, .75, .75)
 end)
-f:RegisterEvent("ADDON_LOADED")
-f:RegisterEvent("PLAYER_LOGIN")
+
+hooksecurefunc('CompactUnitFrame_SetUnit', function (f)
+  f.healthBar:SetStatusBarTexture(.75, .75, .75)
+end)
