f80c1bc4ddc34faf055d62191fa9cce7aa6b3d82
Hao Tran <account@haothitran.com>
2019-09-03 00:07:49 -0400

    Seperate buffs and debuffs

--- Modules/RaidBuffs.lua
+++ Modules/RaidBuffs.lua
@@ -2,14 +2,26 @@
 -- // MODULES / RAID BUFFS
 --------------------------------------------------------------------------------
 
-local debuffSize = 20
-local buffSize = 16
-
-hooksecurefunc('DefaultCompactUnitFrameSetup', function(f)
-  for _, d in ipairs(f.debuffFrames) do
-    d.baseSize = debuffSize
+-- buffs
+hooksecurefunc('CompactUnitFrame_UpdateBuffs', function (f)
+  if not f.buffFrames then return end
+  for i = 1, #f.buffFrames do
+    local buff = f.buffFrames[i]
+    if buff and buff:IsShown() then
+      buff.icon:SetTexCoord(0.07, 0.93, 0.07, 0.93)
+      buff:SetScale(1)
+    end
   end
-  for _, d in ipairs(f.buffFrames) do
-    d:SetSize(buffSize, buffSize)
+end)
+
+-- debuffs
+hooksecurefunc('CompactUnitFrame_UpdateDebuffs', function (f)
+  if not f.debuffFrames then return end
+  for i = 1, #f.debuffFrames do
+    local debuff = f.debuffFrames[i]
+    if debuff and debuff:IsShown() then
+      debuff.icon:SetTexCoord(0.07, 0.93, 0.07, 0.93)
+      debuff:SetScale(1.2)
+    end
   end
 end)
