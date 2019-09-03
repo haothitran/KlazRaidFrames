b95dc491b577b17aad1395fa36ab0ae2edf1b71f
Hao Tran <account@haothitran.com>
2019-09-03 00:30:52 -0400

    Hide group name tags

---
+++ Modules/RaidGroups.lua
@@ -0,0 +1,17 @@
+--------------------------------------------------------------------------------
+-- // MODULES / RAID GROUPS
+--------------------------------------------------------------------------------
+
+-- hide party tag
+hooksecurefunc('CompactPartyFrame_Generate', function ()
+  if CompactPartyFrame and CompactPartyFrame.title then
+    CompactPartyFrame.title:Hide()
+  end
+end)
+
+-- hide group number
+hooksecurefunc('CompactRaidGroup_InitializeForGroup', function (f)
+  if f then
+    f.title:Hide()
+  end
+end)
